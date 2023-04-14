233c8caacd0c0b5e077882abd7fc95b2d4c01019
Dz <MathBeWithCode@gmail.com>
2023-04-14 10:48:34 +0800

    cache module info

--- assets/js/marked-code-doc.js
+++ assets/js/marked-code-doc.js
@@ -9,18 +9,31 @@
       return index;
     },
     tokenizer(src, tokens) {
-      const popupRule = /^@([^\s,]+)/;
+      const popupVarRule = /^@([^\s,]+)/;
+      const popupModRule = /^#([^\s,]+)/;
       let match;
-      if ((match = popupRule.exec(src))) {
+      if ((match = popupVarRule.exec(src))) {
         return {
           type: "code-doc",
           raw: match[0],
           text: match[1].replace(/\\/g, ""),
+          level: "var",
+        };
+      } else if ((match = popupModRule.exec(src))) {
+        return {
+          type: "code-doc",
+          raw: match[0],
+          text: match[1].replace(/\\/g, ""),
+          level: "mod",
         };
       }
     },
     renderer(token) {
-      return `<span class="pop-up" data-id="${token.text}">${token.text}</span>`;
+      if (token.level == "var") {
+        return `<span class="pop-up" data-id="${token.text}">${token.text}</span>`;
+      } else {
+        return `<span class="pop-up" data-path="${token.text}">${token.text}</span>`;
+      }
     },
   };
 
@@ -40,13 +53,29 @@
   // 引用 window.gMarked, 开始的时候是 null
 
   let isLoaded = false;
-  let cacheVar = {};
   const docsCache = {
     cur: null,
     global: [],
     as: {},
     deps: [],
   };
+  const getModContentCached = createCacheFunc(getModContent);
+  const getVarContentCached = createCacheFunc(getVarContent);
+
+  function getContent(elem) {
+    return elem.dataset?.id
+      ? getVarContentCached(elem.dataset.id)
+      : getModContentCached(elem.dataset.path);
+  }
+
+  function clearCache() {
+    docsCache.cur = null;
+    docsCache.global = [];
+    docsCache.as = {};
+    docsCache.deps = [];
+    getModContentCached.clear();
+    getVarContentCached.clear();
+  }
 
   class Container {
     constructor() {
@@ -87,9 +116,7 @@
           top: elemTop,
           height: elemHeight,
         } = elem.getBoundingClientRect();
-        const description = elem.dataset?.id
-          ? getContent(elem.dataset.id)
-          : getModuleContent(elem.dataset.path);
+        const description = getContent(elem);
         this.setContent(description);
         // 需要先显示才能计算出 Client Rect
         this.show();
@@ -105,163 +132,6 @@
         this.container.style.top = `${top}px`;
       };
       elem.addEventListener("mouseover", onMouseOver);
-      function getContent(id) {
-        if (cacheVar?.[id]) {
-          return cacheVar[id];
-        }
-        const content = searchContent(id);
-        cacheVar[id] = content;
-        return content;
-      }
-      function searchContent(id) {
-        if (!isLoaded) return "Loading...<br> Try again";
-
-        {
-          const firstSplit = id.indexOf("-");
-          if (firstSplit != -1) {
-            const aliasName = id.slice(0, firstSplit);
-            const m = docsCache.as?.[aliasName];
-            if (m) {
-              const realId = m.ret + id.slice(firstSplit);
-              const v = m.extern.find((val) => val.id == realId);
-              if (v) {
-                const name = v.id;
-                const virtualName = m.id;
-                const anchor = realId;
-                const content = v.info;
-                return prettyContent(name, virtualName, anchor, content, m);
-              } else return "Not found";
-            }
-          }
-        }
-
-        {
-          const v = docsCache.cur.local.find((val) => val.id == id);
-          if (v) {
-            const name = v.id;
-            const virtualName = docsCache.cur.id;
-            const anchor = name;
-            const content = v.info;
-            return prettyContent(
-              name,
-              virtualName,
-              anchor,
-              content,
-              docsCache.cur
-            );
-          }
-        }
-
-        {
-          const v = docsCache.cur.extern.find((val) => val.id == id);
-          if (v) {
-            const name = v.id;
-            const virtualName = docsCache.cur.id;
-            const anchor = name;
-            const content = v.info;
-            return prettyContent(
-              name,
-              virtualName,
-              anchor,
-              content,
-              docsCache.cur
-            );
-          }
-        }
-
-        {
-          for (const m of docsCache.deps) {
-            const v = m.extern.find((val) => val.id == id);
-            if (v) {
-              const name = v.id;
-              const virtualName = m.id;
-              const anchor = name;
-              const content = v.info;
-              return prettyContent(name, virtualName, anchor, content, m);
-            }
-          }
-        }
-
-        {
-          for (const m of docsCache.global) {
-            const v = m.extern.find((val) => val.id == id);
-            if (v) {
-              const name = v.id;
-              const virtualName = m.id;
-              const anchor = name;
-              const content = v.info;
-              return prettyContent(name, virtualName, anchor, content, m);
-            }
-          }
-        }
-
-        return "Not Found";
-
-        function prettyContent(name, virtualName, anchor, content, m) {
-          return window.gMarked.parse(
-            `${name}: ${getLocation(virtualName, anchor, m)}<br>${content}`
-          );
-        }
-
-        function getLocation(virtualName, anchor, m) {
-          const docsHref = `${getDocsPath(
-            m.repos,
-            virtualName
-          )}#${anchor.toLowerCase()}`;
-          return `<a class="docs" href="${docsHref}">${anchor}</a> <a class="code" href="${getCodePath(
-            m.repos,
-            virtualName
-          )}" target="_blank">code</a>`;
-        }
-      }
-
-      function getModuleContent(mName) {
-        if (!isLoaded) return "Loading...<br> Try again";
-
-        {
-          const m = docsCache.deps.find((m) => m.id == mName);
-          if (m) {
-            const name = m.id;
-            const virtualName = m.id;
-            const content = m.info;
-            return prettyContent(name, virtualName, content, m);
-          }
-        }
-
-        {
-          const m = docsCache.global.find((m) => m.id == mName);
-          if (m) {
-            const name = m.id;
-            const virtualName = m.id;
-            const content = m.info;
-            return prettyContent(name, virtualName, content, m);
-          }
-        }
-
-        {
-          const m = docsCache.as.keys().find((m) => m.id == mName);
-          if (m) {
-            const name = m.id;
-            const virtualName = m.id;
-            const content = m.info;
-            return prettyContent(name, virtualName, content, m);
-          }
-        }
-
-        function prettyContent(name, virtualName, content, m) {
-          return window.gMarked.parse(
-            `${name}: ${getLocation(virtualName, m)}<br>${content}`
-          );
-        }
-
-        function getLocation(virtualName, m) {
-          const docsHref = `${getDocsPath(m.repos, virtualName)}`;
-          return `<a class="docs" href="${docsHref}">${virtualName}</a> <a class="code" href="${getCodePath(
-            m.repos,
-            virtualName
-          )}" target="_blank">code</a>`;
-        }
-      }
     }
 
     setMouseOut(elem) {
@@ -289,7 +159,7 @@
       if (!res) return html;
       isLoaded = false;
       const allRepos = [res.repos, ...remoteRepos];
-      cacheVar = {};
+      clearCache();
       fetchCache(res);
       const viewCode = `[:rocket: VIEW CODE](${getCodePath(
         res.repos,
@@ -310,7 +180,6 @@
         if (!res.ok) return;
         docsCache.cur = JSON.parse(await res.text());
         docsCache.cur.repos = src.repos;
-        docsCache.deps = [];
         for (const virtualName of docsCache.cur.deps) {
           for (const repos of allRepos) {
             const fullPath = getCachePath(repos, virtualName);
@@ -322,7 +191,6 @@
             break;
           }
         }
-        docsCache.global = [];
         for (const virtualName of global) {
           for (const repos of allRepos) {
             const fullPath = getCachePath(repos, virtualName);
@@ -334,7 +202,6 @@
             break;
           }
         }
-        docsCache.as = {};
         for (const [asName, virtualName] of Object.entries(docsCache.cur.as)) {
           for (const repos of allRepos) {
             const fullPath = getCachePath(repos, virtualName);
@@ -359,6 +226,167 @@
 
   function install() {
     docsifyPlugins.push(plugin);
+  }
+
+  function getVarContent(id) {
+    if (!isLoaded) return "Loading...<br> Try again";
+
+    {
+      const firstSplit = id.indexOf("-");
+      if (firstSplit != -1) {
+        const aliasName = id.slice(0, firstSplit);
+        const m = docsCache.as?.[aliasName];
+        if (m) {
+          const realId = m.ret + id.slice(firstSplit);
+          const v = m.extern.find((val) => val.id == realId);
+          if (v) {
+            const name = v.id;
+            const virtualName = m.id;
+            const anchor = realId;
+            const content = v.info;
+            return prettyContent(name, virtualName, anchor, content, m);
+          } else return "Not found";
+        }
+      }
+    }
+
+    {
+      const v = docsCache.cur.local.find((val) => val.id == id);
+      if (v) {
+        const name = v.id;
+        const virtualName = docsCache.cur.id;
+        const anchor = name;
+        const content = v.info;
+        return prettyContent(name, virtualName, anchor, content, docsCache.cur);
+      }
+    }
+
+    {
+      const v = docsCache.cur.extern.find((val) => val.id == id);
+      if (v) {
+        const name = v.id;
+        const virtualName = docsCache.cur.id;
+        const anchor = name;
+        const content = v.info;
+        return prettyContent(name, virtualName, anchor, content, docsCache.cur);
+      }
+    }
+
+    {
+      for (const m of docsCache.deps) {
+        const v = m.extern.find((val) => val.id == id);
+        if (v) {
+          const name = v.id;
+          const virtualName = m.id;
+          const anchor = name;
+          const content = v.info;
+          return prettyContent(name, virtualName, anchor, content, m);
+        }
+      }
+    }
+
+    {
+      for (const m of docsCache.global) {
+        const v = m.extern.find((val) => val.id == id);
+        if (v) {
+          const name = v.id;
+          const virtualName = m.id;
+          const anchor = name;
+          const content = v.info;
+          return prettyContent(name, virtualName, anchor, content, m);
+        }
+      }
+    }
+
+    return "Not Found";
+
+    function prettyContent(name, virtualName, anchor, content, m) {
+      return window.gMarked.parse(
+        `${name}: ${getLocation(virtualName, anchor, m)}<br>${content}`
+      );
+    }
+
+    function getLocation(virtualName, anchor, m) {
+      const docsHref = `${getDocsPath(
+        m.repos,
+        virtualName
+      )}#${anchor.toLowerCase()}`;
+      return `<a class="docs" href="${docsHref}">${anchor}</a> <a class="code" href="${getCodePath(
+        m.repos,
+        virtualName
+      )}" target="_blank">code</a>`;
+    }
+  }
+
+  function getModContent(mName) {
+    if (!isLoaded) return "Loading...<br> Try again";
+
+    {
+      const m = docsCache.deps.find((m) => m.id == mName);
+      if (m) {
+        const name = m.id;
+        const virtualName = m.id;
+        const content = m.info;
+        return prettyContent(name, virtualName, content, m);
+      }
+    }
+
+    {
+      const m = docsCache.global.find((m) => m.id == mName);
+      if (m) {
+        const name = m.id;
+        const virtualName = m.id;
+        const content = m.info;
+        return prettyContent(name, virtualName, content, m);
+      }
+    }
+
+    {
+      const as = docsCache.as;
+      if (!isEmpty(as)) {
+        const m = as.keys().find((m) => m.id == mName);
+        if (m) {
+          const name = m.id;
+          const virtualName = m.id;
+          const content = m.info;
+          return prettyContent(name, virtualName, content, m);
+        }
+      }
+
+      function isEmpty(obj) {
+        return Object.keys(obj).length === 0;
+      }
+    }
+
+    return "Not Found";
+
+    function prettyContent(name, virtualName, content, m) {
+      return window.gMarked.parse(
+        `${name}: ${getLocation(virtualName, m)}<br>${content}`
+      );
+    }
+
+    function getLocation(virtualName, m) {
+      const docsHref = `${getDocsPath(m.repos, virtualName)}`;
+      return `<a class="docs" href="${docsHref}">${virtualName}</a> <a class="code" href="${getCodePath(
+        m.repos,
+        virtualName
+      )}" target="_blank">code</a>`;
+    }
+  }
+
+  function createCacheFunc(func) {
+    let cache = {};
+    const cacheFunc = function (key) {
+      if (cache[key]) return cache[key];
+      const res = func(key);
+      cache[key] = res;
+      return res;
+    };
+    cacheFunc.clear = function () {
+      cache = {};
+    };
+    return cacheFunc;
   }
 
   /**
